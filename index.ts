a8f86e7de456d9d0e1364407a4f13588697f85a1
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2024-04-22 00:55:48 -0400

    done!

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+/*
+* The main program.
+*
+* @author  Emmanuel Fofeyin
+* @version 1.0
+* @since   2024-04-21
+*/
+
+import { Airplane } from './Airplane'
+import { Jet } from './Jet'
+
+const biplane = new Airplane()
+biplane.setSpeed(212)
+console.log(biplane.getSpeed())
+const boeing = new Jet()
+boeing.setSpeed(422)
+console.log(boeing.getSpeed())
+let counter = 0
+while (counter < 4) {
+    boeing.accelerate()
+    console.log(boeing.getSpeed())
+    if (boeing.getSpeed() > 5000) {
+        biplane.setSpeed(biplane.getSpeed() * 2)
+    } else {
+        boeing.accelerate()
+    }
+  counter++
+}
+console.log(biplane.getSpeed())
